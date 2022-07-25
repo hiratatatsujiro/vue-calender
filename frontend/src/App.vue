c3e28edd6a15b14176bce28cc1f24201169ae0d3
平田達二郎 <hiratatoorujirou@hiratatoorujirounoMacBook-Pro.local>
2022-07-25 12:35:18 +0900

    2

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <CalenderPage />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import CalenderPage from "./components/CalenderPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    CalenderPage,
+  },
+};
+</script>
