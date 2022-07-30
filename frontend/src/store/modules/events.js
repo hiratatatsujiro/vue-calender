6faee76c4a339a3bb0ded789df83af7c3ee899c4
平田達二郎 <hiratatoorujirou@hiratatoorujirounoMacBook-Pro.local>
2022-07-31 02:45:42 +0900

    a

--- frontend/src/store/modules/events.js
+++ frontend/src/store/modules/events.js
@@ -1,30 +1,41 @@
 import axios from "axios";
+import { serializeEvent } from "../../functions/serializers";
 
 const apiUrl = "http://localhost:3000";
 
 const state = {
   events: [],
+  event: "",
+  isEditMode: false,
 };
 
 const getters = {
-  events: (state) =>
-    state.events.map((event) => {
-      return {
-        ...event,
-        start: new Date(event.start),
-        end: new Date(event.end),
-      };
-    }),
+  events: (state) => state.events.map((event) => serializeEvent(event)),
+  event: (state) => serializeEvent(state.event),
+  isEditMode: (state) => state.isEditMode,
 };
 
 const mutations = {
   setEvents: (state, events) => (state.events = events),
+  appendEvent: (state, event) => (state.events = [...state.events, event]),
+  setEvent: (state, event) => (state.event = event),
+  setEditMode: (state, bool) => (state.isEditMode = bool),
 };
 
 const actions = {
   async fetchEvents({ commit }) {
     const response = await axios.get(`${apiUrl}/events`);
     commit("setEvents", response.data); // mutationを呼び出す
+  },
+  async createEvent({ commit }, event) {
+    const response = await axios.post(`${apiUrl}/events`, event);
+    commit("appendEvent", response.data);
+  },
+  setEvent({ commit }, event) {
+    commit("setEvent", event);
+  },
+  setEditMode({ commit }, bool) {
+    commit("setEditMode", bool);
   },
 };
 
